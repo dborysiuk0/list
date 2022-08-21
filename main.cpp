2a9fd57585dbc4786f70612e40f2f9721eb26808
Mitia <dborosiuk@gmail.com>
2022-08-21 19:03:07 +0300

    v.01

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+template<typename T>
+class Node{
+public:
+    T value;
+    Node *next;
+};
+template<typename T>
+class LinkedList{
+public:
+    Node<T> *head;
+    LinkedList(){
+       // head = 0;
+        head -> next = new Node<T>;
+    }
+    LinkedList(T data){
+        head -> value = data;
+        head -> next = new Node<T>;
+       
+    }
+    void push_back(T data){
+        Node<T> *new_var = head;
+        while( new_var != 0 && new_var -> next != 0 ){
+            new_var = new_var -> next;
+        }
+         
+        if(new_var){
+            new_var -> next = new Node<T>;
+            new_var -> next -> value = data;
+        } 
+        else{
+           head -> next = new Node<T>;
+           head -> value = data;
+        }
+        
+    }
+    void print(){
+        Node<T> *new_var = head;
+        while( new_var != 0 && new_var -> next != 0 ){
+            new_var = new_var -> next;
+            std::cout << new_var -> value << std::endl;
+        }
+    }
+};
+
+int main(void){
+
+    LinkedList<int> list(10);
+    list.push_back(20);
+    list.push_back(21);
+    list.push_back(21);
+    list.print();
+    return 0;
+}
