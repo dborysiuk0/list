6376b295a90256d940947d4b87f160dd8753cb94
Mitia <daemonburan@outlook.com>
2022-08-23 11:56:29 +0300

    add delete Version 0.1

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ class LinkedList{
 public:
     Node<T> *head = new Node<T>;
     LinkedList(){
-        head = 0;
+        //head = 0;
     }
     LinkedList(T data){
         head -> value = data;      
@@ -20,25 +20,44 @@ public:
         Node<T> *new_var = head;
         while( new_var != 0 && new_var -> next != 0 ){
             new_var = new_var -> next;
-        }
-         
+        }       
         if(new_var){
             new_var -> next = new Node<T>;
             new_var -> next -> value = data;
-        } 
-        else{
-           head -> next = new Node<T>;
-           head -> next -> value = data;
-        }
-        
+        }       
     }
+    void push_front(T data){
+        Node<T> *new_var = new Node<T>; 
+        new_var -> value = data;
+        new_var -> next = head;
+        head = new_var;
+    }
+    void del_index(int index){
+        if(index == 0){
+            Node<T> *new_head = head ->next;
+            delete head;
+            head = new_head;
+        }
+        else{
+            Node<T> *ptr = head;
+            Node<T> *p;
+            int i=0;
+            while ((i+1) != index)
+            {
+                ptr = ptr -> next;
+            }
+            p = ptr -> next;
+            ptr ->next = ptr -> next -> next;
+            delete p;
+        }    
+    }
+
     void print(){
         Node<T> *new_var = head;
-        while( new_var != 0 && new_var -> next != 0 ){
+        while( new_var != 0 ){
             std::cout << new_var -> value << std::endl;
             new_var = new_var -> next;
         }
-        std::cout << new_var -> value << std::endl;
     }
 };
 
@@ -47,7 +66,15 @@ int main(void){
     LinkedList<int> list(10);
     list.push_back(20);
     list.push_back(21);
+    list.push_front(22);
     list.push_back(21);
     list.print();
+    std::cout<<std::endl;
+    list.del_index(1);
+    list.print();
+    std::cout<<std::endl;
+    list.push_back(0);
+    list.print();
+
     return 0;
 }
