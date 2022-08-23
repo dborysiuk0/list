161f035c7969c6d17a21e7b34095602a48a79b7e
Mitia <dborosiuk@gmail.com>
2022-08-23 17:39:03 +0300

    add CMake v0.1

--- main.cpp
+++ main.cpp
@@ -1,54 +1,21 @@
 #include <iostream>
-
-template<typename T>
-class Node{
-public:
-    T value;
-    Node *next;
-};
-template<typename T>
-class LinkedList{
-public:
-    Node<T> *head;
-    LinkedList(){
-        head = 0;
-    }
-    LinkedList(T data){
-        head -> value = data;
-        head -> next = new Node<T>;
-       
-    }
-    void push_back(T data){
-        Node<T> *new_var = head;
-        while( new_var != 0 && new_var -> next != 0 ){
-            new_var = new_var -> next;
-        }
-         
-        if(new_var){
-            new_var -> next = new Node<T>;
-            new_var -> next -> value = data;
-        } 
-        else{
-           head -> next = new Node<T>;
-           head -> value = data;
-        }
-        
-    }
-    void print(){
-        Node<T> *new_var = head;
-        while( new_var != 0 && new_var -> next != 0 ){
-            std::cout << new_var -> value << std::endl;
-            new_var = new_var -> next;
-        }
-    }
-};
+#include "LinkedList.h"
 
 int main(void){
 
-    LinkedList<int> list(10);
-    list.push_back(20);
+    LinkedList<int> list;
+    std::cout<< list.empty()<<std::endl;
     list.push_back(21);
+    list.push_front(22);
     list.push_back(21);
+    list.push_back(22);
+    list.push_back(23);
     list.print();
+    std::cout<<std::endl;
+    list.del_index(5); 
+    list.print();
+    std::cout<< list.empty()<<std::endl;
+    std::cout<< list.size()<<std::endl;
+
     return 0;
 }
